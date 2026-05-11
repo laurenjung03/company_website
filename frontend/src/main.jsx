bba175b3eaff96bfb2dad3b2ec31a588056dc6ce
laurenjung03 <2376267@ewhain.net>
2026-05-11 15:00:16 +0900

    console로 확인중

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import axios from "axios";
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+console.log("API URL:", import.meta.env.VITE_API_URL);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
