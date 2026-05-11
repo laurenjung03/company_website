7edf29c5a371a2426b123d1e6ef67f43c18baa4a
laurenjung03 <2376267@ewhain.net>
2026-05-11 15:17:48 +0900

    로그인-user.js수정

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -96,8 +96,8 @@ router.post("/login", async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
